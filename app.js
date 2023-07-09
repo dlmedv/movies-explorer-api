52767699c4835390c1db355ce24f7c1ffd9ad491
dlmedv <114763924+dlmedv@users.noreply.github.com>
2023-07-09 11:37:38 +0300

    добавляет пакет cors

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const { cors } = require('cors');
 const router = require('./routes');
 const errorMiddlewares = require('./middlewares/errorMiddlewares');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-
+app.use(cors());
 mongoose.connect(DB_ADDRESS, {});
 
 app.use(helmet());
